b9e44b7f5c848cd0e762f10b91a6ec708f627e72
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-08 21:19:57 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -2,7 +2,7 @@ require_relative '../conversions.rb'
 
 describe "conversions" do
   describe '#ounces_to_grams' do
-    it 'given zero, returns 0.0' do
+    it 'given , returns 0.0' do
       grams = ounces_to_grams(0)
       expect(grams).to eq(0.0)
     end
